8d72d97af337ef1cecc2d7cc5e9da592eeea3130
mfrancia <matteo.francia2@studio.unibo.it>
2024-04-05 09:40:46 +0200

    fix(ci): only build the docker image

--- image/oracle11g/f02_oracle_init.sh
+++ image/oracle11g/f02_oracle_init.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+set -exo
 chmod -R 777 /data
 # expdp sales_v1/oracle@xe directory=oracle_dump dumpfile=sales_v1-2023-06-07.dmp SCHEMAS=sales_v1
 # impdp foodmart/oracle@127.0.0.1:1521/xe DIRECTORY=ORACLE_DUMP DUMPFILE=FOODMART.DMP SCHEMAS=foodmart
